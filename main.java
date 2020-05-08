0d17c87de6765b4e7ca4176b60e691217746ef02
Super-rookie-Py <54101813+Super-rookie-Py@users.noreply.github.com>
2020-05-08 22:54:40 +0900

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 2020/05/08 Time to Start. for my dream
+		System.out.println("Hello, World!");
+
+	}
+
+}
